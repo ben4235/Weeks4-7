6dced0c475b655f7acff356f251bfb2982d31195
Benjamin Piatkowski <bpiatkowski02@gmail.com>
2026-02-06 17:29:18 -0500

    WIP

---
+++ Assets/Scripts/objectIdentification.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class objectIdentification : MonoBehaviour
+{
+
+    public GameObject warperObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            isHovering = true;
+        }
+        else
+        {
+            isHovering = false;
+        }
+        bool onHover = !warperObject.activeInHierarchy;
+        bool offHover = warperObject.activeInHierarchy;
+
+        if (shouldTurnOn == true)
+        {
+            warperObject.SetActive(true);
+        }
+
+        if (shouldTurnOff)
+        {
+            warperObject.SetActive(false);
+        }
+    }
+}
